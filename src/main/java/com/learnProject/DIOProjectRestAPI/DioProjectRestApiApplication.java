bc801cc855ba6945ba9051e0ad0d5b5fcab55715
Zh0ny <caio.philipe.queiroz@gmail.com>
2025-04-02 00:41:42 -0300

    Initialize Spring Boot project for the DIO - DECOLATECH 2025 REST API challenge.

---
+++ src/main/java/com/learnProject/DIOProjectRestAPI/DioProjectRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.learnProject.DIOProjectRestAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioProjectRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioProjectRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnProject/DIOProjectRestAPI/DioProjectRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnProject.DIOProjectRestAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioProjectRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
